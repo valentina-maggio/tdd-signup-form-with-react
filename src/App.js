201530128b7fca11d9ab9b5158dd5f36f3c935f3
Valentina <maggio.vale@gmail.com>
2022-05-01 19:48:01 +0100

    Adding input form code to make test pass

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return<div></div>;
+  return<div className='container my-5'>
+    <form>
+      <div className='mb-3'>
+        <label htmlFor='email' className='form-label'>
+          Email address
+        </label>
+        <input 
+          type='email' 
+          id='email' 
+          className='form-control'/>
+      </div>
+    </form>
+  </div>
 }
 
 export default App;
